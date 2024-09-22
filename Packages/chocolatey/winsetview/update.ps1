7bd51bb3e459ed1a5019b5a3223f40ff4e9ff37a
Brian Ferch <bri.ferch@gmail.com>
2024-09-22 03:29:39 -0400

    crazy icon updater

--- Packages/chocolatey/winsetview/update.ps1
+++ Packages/chocolatey/winsetview/update.ps1
@@ -47,6 +47,41 @@ $verificationTemplateContent = Get-Content(Join-Path $currentPath "VERIFICATION.
 $verificationTemplateContent = $verificationTemplateContent -replace "{{VERSION}}", $version -replace "{{CHECKSUM}}", $exeHash
 Write-Host "Updating verification file with version $version and checksum $exeHash"
 
+# Update icon
+try {
+  $icoPath = Join-Path $rootPath "Src\WinSetView.ico"
+  $tempIconsPath = Join-Path $currentPath "tempIcons"
+  New-Item -Path $tempIconsPath -ItemType "directory" | Out-Null
+  magick $icoPath "$tempIconsPath\icon.png"
+  $allIconPngs = Get-ChildItem -Path $tempIconsPath -Include icon-*.png -Recurse
+  $largestIcon
+  foreach ($iconPng in $allIconPngs) {
+    $sizeStr = magick identify -format "%wx%h" $iconPng.FullName
+    $width = [int](($sizeStr -split 'x')[0])
+    if ($width -gt ($largestIcon.width ?? 0)) {
+      $largestIcon = @{
+        image = $iconPng;
+        width = $width;
+        sizeStr = $sizeStr
+      }
+    }
+  }
+  $pngIconPath = "$currentPath\icon.png"
+  if ($largestIcon) {
+    Write-Host "Updating icon $pngIconPath with size $($largestIcon.sizeStr)"
+  } else {
+    throw "Could not create png icon from $icoPath"
+  }
+  if (Test-Path $pngIconPath) {
+    Remove-Item $pngIconPath
+  }
+  Move-Item $largestIcon.image.FullName $pngIconPath
+} finally {
+  if (Test-Path $tempIconsPath) {
+    Remove-Item $tempIconsPath -Recurse
+  }
+}
+
 # Write files
 Set-Content -Path $nuspecPath -Value $newNuspecContent -NoNewline
 Set-Content -Path $verificationPath -Value $verificationTemplateContent
